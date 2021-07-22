18ad2ccf20146a2d438ebca782987e172e699d38
Abanoub Shenouda <Abanoub Shenouda>
2021-07-22 14:31:30 -0400

    Add admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,13 @@
 class Admin::DashboardController < ApplicationController
 
-  
+  #Display a count of how many products are in the database
+  #Display a count of how many categories are in the database
+
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   #protected
   def show
+    @productsCount = Product.all.size
+    @categoriesCount = Category.all.size
+
   end
 end
